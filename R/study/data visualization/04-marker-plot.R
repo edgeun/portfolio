01fc3cd027875d845fe6bbbf8d85dd2dbd5b9144
Donggeun Lee <60956291+edgeun@users.noreply.github.com>
2024-07-29 16:47:08 +0900

    Create 04-marker-plot.R

---
+++ R/study/data visualization/04-marker-plot.R
@@ -0,0 +1,32 @@
+# plotly 패키지 로드
+library(plotly)
+
+# 작업 디렉토리 설정
+setwd("c:\\data")
+
+# 데이터 로드
+car <- read.csv("usedcars.csv", header = TRUE)
+car
+
+# 산점도 생성
+fig <- plot_ly(data = car, 
+               x = ~mileage, 
+               y = ~price, 
+               type = 'scatter', 
+               mode = 'markers', 
+               marker = list(color = 'blue')) 
+
+# 추세선 추가
+fig <- fig %>% add_trace(x = car$mileage, 
+                         y = fitted(lm(price ~ mileage, data = car)), 
+                         type = 'scatter', 
+                         mode = 'lines', 
+                         line = list(color = 'red'), 
+                         name = 'Trend Line')
+
+# 그래프 출력
+fig
+
+# 상관계수값 출력
+cor <- cor(car$mileage, car$price)
+cor
