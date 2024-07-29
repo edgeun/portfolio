63d25b863690b388bf47085db55f67023043c2ec
Donggeun Lee <60956291+edgeun@users.noreply.github.com>
2024-07-29 22:09:22 +0900

    Update oracle-DB-connect.R

--- R/study/oracle-DB-connect.R
+++ R/study/oracle-DB-connect.R
@@ -10,8 +10,8 @@ library(RJDBC)
 driver <- JDBC('oracle.jdbc.driver.OracleDriver', 'c:\\data\\ojdbc8.jar') 
 
 oracle_db <- dbConnect(driver, 'jdbc:oracle:thin:@127.0.0.1:1521/xe', 'c##scott','tiger')
-# 오라클DB에 접속하기 위한 ip주소, 포트번호, 서비스 이름 입력
-# 오라클DB 사용자 유저 이름, 비밀번호 입력
+# 오라클 DB에 접속하기 위한 ip주소, 포트번호, 서비스 이름 입력
+# 오라클 DB 사용자 유저 이름, 비밀번호 입력
 
 emp_query <- 'select e.ename, d.loc
                 from emp  e, dept  d
