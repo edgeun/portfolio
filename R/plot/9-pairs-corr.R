a6884ebdfec70b59ba72600eeff8c7b6b0ccb393
Donggeun Lee <60956291+edgeun@users.noreply.github.com>
2024-11-06 23:39:56 +0900

    Delete R/plot directory

--- R/plot/9-pairs-corr.R
+++
@@ -1,9 +0,0 @@
-# 패키지 설치
-# install.packages("psych")
-library(psych)
-
-# 데이터 불러오기 (data: challenger.csv)
-cha <- read.csv("c:\\data\\challenger.csv", header = T)
-
-# pairs 패키지를 이용하여 종속변수와 독립변수들끼리의 상관관계 확인
-pairs.panels(cha, pch = '.')
