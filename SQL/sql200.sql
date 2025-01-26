bad8624f58b4518427a0d0a1ee092baac9591682
Donggeun Lee <60956291+edgeun@users.noreply.github.com>
2025-01-27 01:22:44 +0900

    Update sql200.sql

--- SQL/sql200.sql
+++ SQL/sql200.sql
@@ -44,3 +44,72 @@ INSERT INTO EMP VALUES (7876,'ADAMS','CLERK',7788,'83-01-15',1100,NULL,20);
 INSERT INTO EMP VALUES (7934,'MILLER','CLERK',7782,'82-01-11',1300,NULL,10);
 
 commit;
+
+
+-- 모든 열 출력
+SELECT *
+ FROM emp;
+ 
+SELECT empno, ename, job, mgr, hiredate, sal, comm, deptno
+ FROM emp;
+ 
+-- 컬럼 별칭
+SELECT empno AS 사원번호, ename AS 사원이름, sal AS "Salary"
+ FROM emp;
+ 
+-- 연결 연산자
+SELECT ename || sal -- mysql에서는 concat 함수 사용 ||은 or 연산자
+ FROM emp;
+
+SELECT CONCAT(ename, '의 월급은 ', sal, ' 입니다.') AS "사원의 월급 정보"
+ FROM emp;
+ 
+-- 중복 데이터 제거해서 출력
+SELECT DISTINCT job
+ FROM emp;
+ 
+-- 데이터 정렬 
+SELECT ename, sal
+ FROM emp
+ ORDER BY sal ASC;
+ 
+SELECT ename, sal AS 월급
+ FROM emp
+ ORDER BY 월급 ASC;
+ 
+SELECT ename, deptno, sal
+ FROM emp
+ ORDER BY deptno ASC, sal DESC;
+ 
+SELECT ename, deptno, sal
+ FROM emp
+ ORDER BY 2 ASC, 3 DESC;
+
+-- WHERE절
+SELECT ename, sal, job
+ FROM emp
+ WHERE sal = 3000;
+ 
+SELECT ename AS 이름, sal AS 월급
+ FROM emp
+ WHERE sal >= 3000;
+ 
+SELECT ename, sal, job, hiredate, deptno
+ FROM emp
+ WHERE ename='SCOTT';
+ 
+SELECT ename, hiredate
+ FROM emp
+ WHERE hiredate = '81/11/17';
+ 
+SELECT *
+ FROM NLS_SESSION_PARAMETERS
+ WHERE PARAMETER = 'NLS_DATE_FORMAT';
+
+ALTER SESSION SET NLS_DATE_FORMAT='YY/MM/DD';  -- 오라클 SQL -- 연도 RR(50년 기준 전/현/다음세기 출력), YY(현재세기) 구분 주의
+ 
+SELECT @global.date_format;
+
+SET @session.date_format = '%Y-%m-%d %H:%i:%s';  -- mySQL -- y(소문자): 두자리 연도, Y(대문자): 네자리 연도
+
+SELECT @session.date_format;  -- %Y-%m-%d %H:%i:%s
